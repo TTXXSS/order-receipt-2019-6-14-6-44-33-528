363b844a77e0fbaa685d9a787852c14b0a6685fc
TTXXSS <1374230455@qq.com>
2019-07-15 20:18:52 +0800

    update:constructor LineItem in class LineItem delete the super()

--- src/main/java/org/katas/refactoring/LineItem.java
+++ src/main/java/org/katas/refactoring/LineItem.java
@@ -6,25 +6,28 @@ public class LineItem {
     private int qty;
 
     public LineItem(String desc, double p, int qty) {
-        super();
         this.desc = desc;
         this.p = p;
         this.qty = qty;
     }
 
     public String getDescription() {
+
         return desc;
     }
 
     public double getPrice() {
+
         return p;
     }
 
     public int getQuantity() {
+
         return qty;
     }
 
     double totalAmount() {
+
         return p * qty;
     }
 }
